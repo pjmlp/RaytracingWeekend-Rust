bf0a322e2c84b6657b11341e3cfe0cbc8d047cfc
pjmlp <pjmlp@progtools.org>
2022-01-23 12:56:00 +0100

    Fixing Shadow Acne. Chapter 8.4

--- OneWeekend/libs/utils/src/lib.rs
+++ OneWeekend/libs/utils/src/lib.rs
@@ -43,7 +43,7 @@ pub fn ray_color(r: Ray, world : &dyn Hitable, depth: i32) -> Color {
         return Color::new(0.0, 0.0, 0.0);
     }
 
-    if world.hit(&r, 0.0, f64::INFINITY, &mut rec) {
+    if world.hit(&r, 0.001, f64::INFINITY, &mut rec) {
         let target = rec.p + rec.normal + random_unit_vector();
         return 0.5 * ray_color(Ray::new(rec.p, target - rec.p), world, depth - 1);
     }
