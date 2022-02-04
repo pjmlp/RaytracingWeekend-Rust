6c245334591fd9cd4ae4f72c83e0f97dbaa2a30e
pjmlp <pjmlp@progtools.org>
2022-02-04 22:09:01 +0100

    Chapter 9.6, fuzzy reflection

--- libs/utils/src/material.rs
+++ libs/utils/src/material.rs
@@ -30,12 +30,13 @@ impl Material for Lambertian {
 }
 
 pub struct Metal {
-    pub albedo : Color
+    pub albedo : Color,
+    pub fuzz : f64
 }
 
 impl Metal {
-    pub fn new(a : Color) -> Self {
-        Metal { albedo: a }
+    pub fn new(a : Color, f : f64) -> Self {
+        Metal { albedo: a, fuzz: if f < 1.0 { f } else  { 1.0 } }
     }
 }
 
@@ -43,7 +44,7 @@ impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
         let reflected = reflect(unit_vector(r_in.direction()), rec.normal);
 
-        *scattered = Ray::new(rec.p, reflected);
+        *scattered = Ray::new(rec.p, reflected + self.fuzz * random_in_unit_sphere());
         *attenuation = self.albedo;
 
         scattered.direction().dot(rec.normal) > 0.0
--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,8 @@ fn main() -> Result<(), Error> {
 
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
     
     world.add(Rc::new(Sphere::new(DVec3::new( 0.0, -100.5, -1.0), 100.0, Some(material_ground))));
     world.add(Rc::new(Sphere::new(DVec3::new( 0.0,    0.0, -1.0),   0.5, Some(material_center))));
